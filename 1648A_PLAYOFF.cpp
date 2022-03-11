df478b6e463e0ad47a8fdd3a06db3056a47a5c66
Jack Nguyen <79352883+Cuberates@users.noreply.github.com>
2022-03-11 14:00:54 +0700

    Create 1648A_PLAYOFF.cpp

---
+++ 1648A_PLAYOFF.cpp
@@ -0,0 +1,20 @@
+// 1648A_PLAYOFF
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution() { 
+	long long N; cin >> N;
+	// The answer is 2^n-1, from simple observations
+	cout << fixed << setprecision(0) << pow(2,N)-1 << "\n";
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt; cin >> tt;
+	for(int i=0;i<tt;i++) {
+		// cout << "TEST CASE #" << i << " : \n"; // For debugging purposes
+		solution();
+	}
+}
